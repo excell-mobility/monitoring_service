f9738df75cb629f62591fd24f6e1271652ba9906
Pieper, Stephan <spieper@beuth-hochschule.de>
2015-12-21 17:07:46 +0100

    refactoring

--- src/main/java/monitoring/MonitoringService.java
+++ src/main/java/monitoring/MonitoringService.java
@@ -2,11 +2,15 @@ package monitoring;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 import org.json.JSONArray;
 
 import rest.CalendarConnector;
+import beans.CalendarAppointment;
 import calendarextraction.AppointmentExtraction;
+
+import com.google.common.collect.Maps;
 
 public class MonitoringService {
 
@@ -15,14 +19,18 @@ public class MonitoringService {
 		while(true) {
 			
 			try {
+				
 				AppointmentExtraction extraction = new AppointmentExtraction();
 				Date currentDate = new Date();
+				Map<String, List<CalendarAppointment>> appointmentsPerCalendar = Maps.newHashMap();
 				
 				// 1: get calendars and observe the appointments
 				JSONArray calendarUsers = CalendarConnector.getCalendarUsers();
 				List<String> extractCalendarUsers = extraction.extractCalendarUsers(calendarUsers);
 				for(String userCalendar: extractCalendarUsers) {
-					
+					JSONArray appointmentsForCalendar = CalendarConnector.getAppointmentsForCalendar(userCalendar);
+					List<CalendarAppointment> extractAppointments = extraction.extractAppointments(appointmentsForCalendar);
+					appointmentsPerCalendar.put(userCalendar, extractAppointments);
 				}
 				
 				// 2: extract the current location of all staff members
@@ -31,8 +39,8 @@ public class MonitoringService {
 				
 				// 4: remind the customer, when the staff arrives or if staff is too late
 				
-				// sleep for 10 seconds
-				Thread.sleep(10000);
+				// sleep for 5 minutes
+				Thread.sleep(300000);
 			} catch(Exception e) {
 				System.out.println("API problems, let's try again");
 				System.out.println(e);
